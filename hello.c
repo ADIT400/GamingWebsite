5efd808ae710910753398070027af87b429f079c
ADIT400 <adityadangi1920@gmail.comm>
2025-06-11 11:23:31 +0530

    remember

--- hello.c
+++ hello.c
@@ -3,5 +3,6 @@ int main() {
     printf("Hello, World!\n");
     printf("Welcome to the Gaming Website!\n");
     printf("Enjoy your stay and have fun gaming!\n");
+    printf("Remember to follow the community guidelines.\n");
     return 0;
 }
